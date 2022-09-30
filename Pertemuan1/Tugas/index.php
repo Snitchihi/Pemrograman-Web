8e4ddc764632697350ddb82881ac956b831decc6
Snitch <marcelyukie@gmail.com>
2022-09-30 08:01:15 +0700

    update footer

--- Pertemuan1/Tugas/index.php
+++ Pertemuan1/Tugas/index.php
@@ -6,15 +6,8 @@ include "konten.html";
 
 
 <div class="card">
-        <div class="card-header">
-            Quote
-        </div>
         <div class="card-body">
-            <blockquote class="blockquote mb-0">
-                <p>A well-known quote, contained in a blockquote element.</p>
-                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite>
-                </footer>
-            </blockquote>
+                <p>Copyright ⒸWhale Industry All rights reserved | Created by <a href="https://www.instagram.com/pratamayukie.a/?next=%2F&hl=id"> Someone who love you sincerly</a></p>
         </div>
     </div>
 
